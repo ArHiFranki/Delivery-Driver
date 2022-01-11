ad141b47d3cb3636d51cea6dfe6b416b948d5317
ArHiFranki <franki550@gmail.com>
2022-01-11 23:32:51 +0700

    Добавил объект плеера и скрипт для базового перемещения

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float steerSpeed = 0.1f;
+    [SerializeField] private float moveSpeed = 0.01f;
+
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        transform.Rotate(0, 0, steerSpeed);
+        transform.Translate(0, moveSpeed, 0);
     }
 }
