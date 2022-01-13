bc76ae1aa405ab01c3a9532532c8695b4d6af042
ArHiFranki <franki550@gmail.com>
2022-01-13 15:16:49 +0700

    Добавил систему ускорения и замедления

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] private float turnSpeed = 0.1f;
-    [SerializeField] private float moveSpeed = 0.01f;
-
-    private void Start()
-    {
-        
-    }
+    [SerializeField] private float turnSpeed = 300f;
+    [SerializeField] private float moveSpeed = 18f;
+    [SerializeField] private float slowSpeed = 12f;
+    [SerializeField] private float boostSpeed = 22f;
 
     private void Update()
     {
@@ -18,5 +15,18 @@ public class Driver : MonoBehaviour
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -turnAmount);
         transform.Translate(0, moveAmount, 0);
+    }
+
+    private void OnCollisionEnter2D()
+    {
+        moveSpeed = slowSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "SpeedUp")
+        {
+            moveSpeed = boostSpeed;
+        }
     }
 }
