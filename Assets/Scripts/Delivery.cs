22d6d7e6699d7ecaf70c5bdcf0e7d6c4fd2fa3b8
ArHiFranki <franki550@gmail.com>
2022-01-13 14:28:47 +0700

    Доработал логики доставки посылок, добавил уничтожение посылок после доставки

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] private float destroyDelay = 0.5f;
+
     private bool hasPackage;
 
     private void OnCollisionEnter2D()
@@ -13,10 +15,11 @@ public class Delivery : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Package")
+        if (collision.tag == "Package" && !hasPackage)
         {
             Debug.Log("Package pick up!");
             hasPackage = true;
+            Destroy(collision.gameObject, destroyDelay);
         }
 
         if (collision.tag == "Customer" && hasPackage)
