4a01a38470e2149a494d50d80d52e0e77d9809d1
ArHiFranki <franki550@gmail.com>
2022-01-13 11:33:18 +0700

    Добавил триггер и обработку взаимодействия с ним

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -8,4 +8,9 @@ public class Collision : MonoBehaviour
     {
         Debug.Log("Collision Enter");
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("On Trigger Enter");
+    }
 }
